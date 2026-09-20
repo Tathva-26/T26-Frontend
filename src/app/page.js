cc2ed7c6a2669f70038eff801cc2cf2a17a5eada
dhiyaroopyabr <dhiyaroopyabr@gmail.com>
2026-09-20 22:05:56 +0530

    feat: implemented menu for windows view

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,5 @@
-// import TechConclave from "@/pageComponents/TechConclave";
-
-// export default function Page() {
-//   return (
-//     <main>
-//       <TechConclave />
-//     </main>
-//   );
-// }
-
-import TathvaMenu from "@/components/TathvaMenu/TathvaMenu";
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#1b1b1b]">
-      <TathvaMenu />
-    </main>
+    
   );
 }
