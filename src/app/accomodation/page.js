ff486b1df9c4015f4f13ac271b39d80ec4451371
Tejas Shyam <tejasshyampn@gmail.com>
2026-09-25 21:56:53 +0530

    added accomodation page

---
+++ src/app/accomodation/page.js
@@ -0,0 +1,10 @@
+import Accomodation from "@/pageComponents/Accomodation";
+
+export const metadata = {
+  title: "Accomodation - Tathva '26",
+  description: "Find a place to stay during Tathva '26",
+};
+
+export default function AccomodationPage() {
+  return <Accomodation />;
+}
