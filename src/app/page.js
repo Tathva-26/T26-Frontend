08ba5d4239ee8ab6750c369a30e454a2fbb1df26
souparnikavp17 <souparnikavp17@gmail.com>
2026-09-27 00:46:32 +0530

    fix: add default export for Lead page component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,3 @@
-import TathvaMenu from "@/components/TathvaMenu/TathvaMenu";
-
 export default function Home() {
-  return (
-    <main>
-      <TathvaMenu />
-      {/* rest of your homepage content */}
-    </main>
-  );
+  return (<> </>);
 }
